45636a6996cde56b801abb38033f45d3fb45e832
Ishan (Anant) Gupta <132544766+ISHOOO@users.noreply.github.com>
2024-07-05 13:06:11 +0530

    Update hourglass.py
    convert multiline for loop to single line

--- patterns/hourglass.py
+++ patterns/hourglass.py
@@ -1,6 +1,5 @@
 n=eval(input("Enter a number: "))
-for i in range(2*n-1):
-    print(" "*i+"* "*(n-i)) if i<n else print(" "*(2*n-i-2)+"* "*(i-n+2))
+print(" "*i+"* "*(n-i)) if i<n else print(" "*(2*n-i-2)+"* "*(i-n+2)) for i in range(2*n-1)
 """
 * * * * * * 
  * * * * * 
