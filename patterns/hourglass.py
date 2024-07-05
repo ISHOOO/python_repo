9f01518458b88f3f5c4f36ddc870667052277c96
Ishan (Anant) Gupta <132544766+ISHOOO@users.noreply.github.com>
2024-07-05 13:03:50 +0530

    Update hourglass.py
    convert multiline if else to single line if else

--- patterns/hourglass.py
+++ patterns/hourglass.py
@@ -1,9 +1,6 @@
 n=eval(input("Enter a number: "))
 for i in range(2*n-1):
-    if i<n:
-        print(" "*i+"* "*(n-i))
-    else:
-        print(" "*(2*n-i-2)+"* "*(i-n+2))
+    print(" "*i+"* "*(n-i)) if i<n else print(" "*(2*n-i-2)+"* "*(i-n+2))
 """
 * * * * * * 
  * * * * * 
